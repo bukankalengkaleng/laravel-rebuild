66996e6ed29a5b2f04aa98e5863b23b849630bb5
R. Kukuh <rkukuh@gmail.com>
2018-12-28 23:37:53 +0700

    Define logic

--- src/Console/Commands/Rebuild.php
+++ src/Console/Commands/Rebuild.php
@@ -2,6 +2,7 @@
 
 namespace BukanKalengKaleng\LaravelRebuild\Console\Commands;
 
+use App;
 use Illuminate\Console\Command;
 
 class Rebuild extends Command
@@ -18,7 +19,7 @@ class Rebuild extends Command
      *
      * @var string
      */
-    protected $description = 'Rebuild your app from scratch';
+    protected $description = 'Rebuild the app from scratch';
 
     /**
      * Create a new command instance.
@@ -37,6 +38,66 @@ class Rebuild extends Command
      */
     public function handle()
     {
-        $this->line('Rebuild in progress...');
+        if (App::environment(['prod', 'production'])) {
+            if ($this->confirm('You are in PRODUCTION environment. Continue?')) {
+                $this->commandOperations();
+
+                return;
+            }
+        }
+
+        $this->commandOperations();
+    }
+
+    /**
+     * Command operation sequence
+     *
+     * @return mixed
+     */
+    protected function commandOperations()
+    {
+        $this->line('');
+        $this->call('migrate:fresh');
+        $this->line('');
+
+        $this->info('[START] Flush the application cache..........');
+        $this->callSilent('cache:clear');
+        $this->info('[DONE ] Flush the application cache.');
+        $this->line('');
+
+        $this->info('[START] Remove the configuration cache file..........');
+        $this->callSilent('config:clear');
+        $this->info('[DONE ] Remove the configuration cache file.');
+        $this->line('');
+
+        $this->info('[START] Remove the route cache file..........');
+        $this->callSilent('route:clear');
+        $this->info('[DONE ] Remove the route cache file.');
+        $this->line('');
+
+        $this->info('[START] Clear all compiled view files..........');
+        $this->callSilent('view:clear');
+        $this->info('[DONE ] Clear all compiled view files.');
+        $this->line('');
+
+        $this->info('[START] Flush expired password reset tokens..........');
+        $this->callSilent('auth:clear-resets');
+        $this->info('[DONE ] Flush expired password reset tokens.');
+        $this->line('');
+
+        $this->info('[START] Clear compiled class files..........');
+        $this->callSilent('clear-compiled');
+        $this->info('[DONE ] Clear compiled class files.');
+        $this->line('');
+
+        $this->info('[START] Rebuild the cached package manifest..........');
+        $this->callSilent('package:discover');
+        $this->info('[DONE ] Rebuild the cached package manifest.');
+        $this->line('');
+
+        $this->info('[START] Create a symbolic link..........');
+        $this->callSilent('storage:link');
+        $this->info('[DONE ] Create a symbolic link.');
+        $this->line('');
     }
 }
