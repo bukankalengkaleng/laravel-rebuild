eda2b54d7331e1f9702f324cda7ffbecc629b61a
R. Kukuh <rkukuh@gmail.com>
2020-03-15 17:13:57 +0700

    Spacing

--- config/rebuild.php
+++ config/rebuild.php
@@ -4,6 +4,7 @@ return [
     /**
      * Specify what rebuilding process should run
      */
+
     'should_rebuild_database_schema' => true,
     'should_seed_initial_data' => true,
 
@@ -17,13 +18,15 @@ return [
         'seeder_name' => 'ExampleDataSeeder',
     ],
 
-    'should_clear_cache' => true,
-    'should_celar_config' => true,
-    'should_clear_route' => true,
     'should_clear_view' => true,
-    'should_flush_expired_password_reset_token' => true,
-    'should_clear_compiled_classes' => true,
+    'should_clear_cache' => true,
+    'should_clear_route' => true,
+    'should_celar_config' => true,
     'should_rediscover_packages' => true,
     'should_create_symbolic_link' => true,
+    'should_clear_compiled_classes' => true,
+    'should_flush_expired_password_reset_token' => true,
+
+    // Run 'laravel-self-diagnosis' by BeyondCode
     'should_self_diagnosis' => true,
 ];
